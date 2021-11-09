90473a1652ec70bc6e3cfd81c71eaa2980b12e6f
Hunt <71895829+AnonymousErrors@users.noreply.github.com>
2021-11-09 13:00:13 +0200

    Create content.js

---
+++ content.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    if(document.title.toLowerCase().search("discord nitro")>-1){
+        document.body.innerHTML = "<h1 style=\"color:red;font-size:100px;\">You might be attempting to enter a free discord nitro scam, stop right now!<h1>"
+    }
+}
